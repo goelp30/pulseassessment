20304b4962bdc6306d55bedec8e49545df31ecb4
Bhupendra Pratap Maurya <bhupendra.maurya@admiralindia.com>
2024-11-22 15:23:25 +0530

    tailwind added and assessment mock data created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,5 +11,6 @@ export const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
     { path: 'register', component: RegisterComponent },
     { path: 'generatelink', component: GenerateLinkComponent },
+    { path: 'generatelink/modelpopup', component: GenerateLinkComponent },
     { path: '**', component: DashboardComponent, canActivate: [AuthGuard] }
 ];
--- src/app/components/generate-link/generate-link.component.ts
+++ src/app/components/generate-link/generate-link.component.ts
@@ -1,41 +1,40 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import assessmentData from '../../assets/assessment_mockData.json';
 
 @Component({
   selector: 'app-generate-link',
   standalone: true,
-  imports: [NgFor, FormsModule,],
+  imports: [NgFor, FormsModule],
   templateUrl: './generate-link.component.html',
-  styleUrl: './generate-link.component.css'
+  styleUrl: './generate-link.component.css',
 })
-export class GenerateLinkComponent {
-  assessments = [
-    { id: 1, title: 'JavaScript Basics' },
-    { id: 2, title: 'React Components' },
-    { id: 3, title: 'Database Design' },
-    { id: 4, title: 'Algorithms and Data Structures' },
-    { id: 5, title: 'Web Security Principles' }
-  ];
-
+export class GenerateLinkComponent implements OnInit {
+  assessments: any[] = [];
+  filteredAssessments: any[] = [];
   selectedLink: string = '';
   searchQuery: string = '';
-  filteredAssessments = [...this.assessments];
+  
   @ViewChild('modalLink') modalLink!: ElementRef;
 
+  ngOnInit(): void {
+    this.assessments = assessmentData;
+    this.filteredAssessments = [...this.assessments]; 
+  }
+
   filterAssessments(): void {
-    this.filteredAssessments = this.assessments.filter(assessment =>
-      assessment.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+    this.filteredAssessments = this.assessments.filter((assessment) =>
+      assessment.assessmentText.toLowerCase().includes(this.searchQuery.toLowerCase())
     );
   }
-  
+
   generateLink(id: number): string {
     return `https://example.com/assessments/${id}`;
   }
 
   openModal(link: string): void {
     this.selectedLink = link;
-    const modal = document.getElementById('modalLink');
     if (this.modalLink) {
       this.modalLink.nativeElement.style.display = 'block';
     }
