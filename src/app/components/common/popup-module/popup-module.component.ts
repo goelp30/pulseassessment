c8894029e6a532129b128334b12627f2eac65eb5
Mradroit611 <sanjayvermaa12121@gmail.com>
2024-11-26 10:55:56 +0530

    common component created

---
+++ src/app/components/common/popup-module/popup-module.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-popup-module',
+  standalone: true,
+  imports: [],
+  templateUrl: './popup-module.component.html',
+  styleUrl: './popup-module.component.css'
+})
+export class PopupModuleComponent {
+
+}
