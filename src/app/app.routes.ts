8f4243baee06a73edb5da22bd9f94d9878a58fba
Alka167 <alkayadav89290084@gmail.com>
2024-11-27 16:11:31 +0530

    evaluation commom module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { AuthGuard } from '@angular/fire/auth-guard';
 import { EvaluationDashboardComponent } from './components/evaluation/evaluation-dashboard/evaluation-dashboard.component';
 import { ViewAssessmentComponent } from './components/evaluation/view-assessment/view-assessment.component';
 import { EvaluateAssessmentComponent } from './components/evaluation/evaluate-assessment/evaluate-assessment.component';
+import { QuestionsComponent } from './components/evaluation/commonEvaluationComponent/questions/questions.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -22,6 +23,7 @@ export const routes: Routes = [
   },
   { path: 'view', component: ViewAssessmentComponent },
   { path: 'evaluate', component: EvaluateAssessmentComponent },
+  // { path: 'common', component: QuestionsComponent },
 
   { path: '**', component: DashboardComponent, canActivate: [AuthGuard] },
 ];
