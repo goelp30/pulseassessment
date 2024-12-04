9fdf4e13201f630a83f073a3e58b187bacebbda9
Mayank Singh Rathore <mayank.rathore@admiralindia.com>
2024-12-04 10:54:15 +0530

    Improved Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,18 +4,16 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AuthGuard } from '@angular/fire/auth-guard';
 import { EvaluationDashboardComponent } from './components/evaluation/evaluation-dashboard/evaluation-dashboard.component';
-import { ViewAssessmentComponent } from './components/evaluation/view-assessment/view-assessment.component';
+
 import { EvaluateAssessmentComponent } from './components/evaluation/evaluate-assessment/evaluate-assessment.component';
-import { QuestionsComponent } from './components/evaluation/commonEvaluationComponent/questions/questions.component';
-import { MarksComponent } from './components/evaluation/commonEvaluationComponent/marks/marks.component';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard],},
   { path: 'register', component: RegisterComponent },
-  {path: 'evaluation', component: EvaluationDashboardComponent,},
-  { path: 'evaluation/view/:id', component: ViewAssessmentComponent },
-  { path: 'evaluation/assessment/:id', component: EvaluateAssessmentComponent },
-  { path: '**', component: DashboardComponent, canActivate: [AuthGuard] },
+  {path: 'evaluation', component: EvaluationDashboardComponent},
+  {path:'evaluate',component:EvaluateAssessmentComponent}
+  ,{ path: '**', component: DashboardComponent, canActivate: [AuthGuard] },
 ];
