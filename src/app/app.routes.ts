f581dca96ee2de5c392310df7e280adcff14f4f7
Bhupendra Pratap Maurya <bhupendra.maurya@admiralindia.com>
2024-12-07 01:48:03 +0530

    dynaimic route is removed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,16 +5,22 @@ import { RegisterComponent } from './components/register/register.component';
 import { AuthGuard } from '@angular/fire/auth-guard';
 import { GenerateLinkComponent } from './components/linkGeneration/generate-link/generate-link.component';
 import { AssessmentFormComponent } from './components/assessment-form/assessment-form.component';
+import { AssessmentManagerComponent } from './components/linkGeneration/assessment-manager/assessment-manager.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent },
-    { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-    { path: 'register', component: RegisterComponent },
-    { path: 'generatelink', component: GenerateLinkComponent,children:[
-        { path: ':assessmentName', component: GenerateLinkComponent },
-    ] }, 
-    {path:'assessmentForm',component:AssessmentFormComponent},
-   
-    { path: '**', component: DashboardComponent, canActivate: [AuthGuard] }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'generatelink',
+    component: GenerateLinkComponent
+  },
+  { path: 'assessmentForm', component: AssessmentFormComponent },
+  { path: 'assessmentManager', component: AssessmentManagerComponent },
+  { path: '**', component: DashboardComponent, canActivate: [AuthGuard] },
 ];
