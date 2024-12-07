6df8c91b9740a4a05c810433287c3abcef9d56e2
Bhupendra Pratap Maurya <bhupendra.maurya@admiralindia.com>
2024-12-07 13:51:15 +0530

    expired link page addedd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { AuthGuard } from '@angular/fire/auth-guard';
 import { GenerateLinkComponent } from './components/linkGeneration/generate-link/generate-link.component';
 import { AssessmentRecordsComponent } from './components/linkGeneration/assessment-records/assessment-records.component';
+import { LinkExpiredComponent } from './components/linkGeneration/link-expired/link-expired.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -17,8 +18,9 @@ export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   {
     path: 'generatelink',
-    component: GenerateLinkComponent
+    component: GenerateLinkComponent,
   },
   { path: 'assessmentrecords', component: AssessmentRecordsComponent },
+  { path: 'link-expired', component: LinkExpiredComponent },
   { path: '**', component: DashboardComponent, canActivate: [AuthGuard] },
 ];
