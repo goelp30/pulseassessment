0a8534c8e3d1fd3985dbbb0c1a9e44074b338787
Mradroit611 <sanjayvermaa12121@gmail.com>
2024-12-09 20:39:40 +0530

    Quiz routes removed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,6 @@ import { RegisterComponent } from './components/register/register.component';
 import { AuthGuard } from '@angular/fire/auth-guard';
 import { AssessmentRecordsComponent } from './components/linkGeneration/Pages/assessment-records/assessment-records.component';
 import { LinkExpiredComponent } from './components/linkGeneration/Pages/link-expired/link-expired.component';
-import { QuizDashboardComponent } from './components/QUIZ/quiz-dashboard/quiz-dashboard.component';
-import { TermsConditionsComponent } from './components/QUIZ/terms-conditions/terms-conditions.component';
 import { AlreadyAttendedComponent } from './components/linkGeneration/Pages/already-attended/already-attended.component';
 import { LinkGenerationComponent } from './components/linkGeneration/link-generation/link-generation.component';
 
@@ -25,8 +23,6 @@ export const routes: Routes = [
   { path: 'linkexpired', component: LinkExpiredComponent },
   { path: 'alreadyattended', component: AlreadyAttendedComponent },
   // Quiz Module Routes 
-  { path: 'termsandconditions', component: TermsConditionsComponent }, //Replace with terms and conditions
-  { path: 'quiz', component: QuizDashboardComponent },
   
   { path: '**', component: DashboardComponent, canActivate: [AuthGuard] },
 ];
