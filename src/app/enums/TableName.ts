c24cfb189c3267813f77fd0617d8710cd74369b0
Krishan_Chamoli <krishanchamoli46@gmail.com>
2024-12-12 12:20:58 +0530

    table name added

--- src/app/enums/TableName.ts
+++ src/app/enums/TableName.ts
@@ -1,8 +1,8 @@
 export const enum TableNames {
-
     Employee = 'employee',
     Candidate = 'candidate',
     Subject = 'subject',
     Assessment = 'assessment',
-    AssessmentList='assessmentList'
+    AssessmentList='assessmentList',  
+    Question = "question",
 }
