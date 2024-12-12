c94bc95d8a8a88fcdc66eea04d505b71be5a1a28
harshjdev <harsh97736@gmail.com>
2024-12-12 12:01:15 +0530

    tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+ 
 const withMT = require("@material-tailwind/html/utils/withMT");
-
+ 
 module.exports = withMT({
   content: ["./src/**/*.{html,ts}"],
   theme: {
@@ -7,3 +9,4 @@ module.exports = withMT({
   },
   plugins: [],
 });
+ 
