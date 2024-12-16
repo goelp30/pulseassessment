619a783706dc8f80a1790e909d9a7a010c373fcd
Bhupendra Pratap Maurya <bhupendra.maurya@admiralindia.com>
2024-12-16 19:45:33 +0530

    auth guard for T&c

---
+++ src/app/auth/link-status.guard.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { FireBaseService } from '../../sharedServices/FireBaseService';
+import { assessmentRecords } from '../models/assessmentRecords';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LinkStatusGuard implements CanActivate {
+  constructor(
+    private firebaseService: FireBaseService<assessmentRecords>,
+    private router: Router
+  ) {}
+
+  canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
+    const userId = route.params['userId'];
+    const assessmentId = route.params['assessmentId'];
+
+    return this.firebaseService.getAllData('assessmentRecords').pipe(
+      map((data: assessmentRecords[]) => {
+        const assessmentRecord = data.find(
+          (assessment) =>
+            assessment.userId === userId &&
+            assessment.assessmentId === assessmentId
+        );
+
+        if (!assessmentRecord) {
+          this.router.navigate(['/invalid']);
+          return false;
+        }
+
+        const { expiryDate, isAccessed, isValid } = assessmentRecord;
+
+        if (this.isLinkExpired(expiryDate)) {
+          this.router.navigate(['/linkexpired']);
+          return false;
+        } else if (isValid && isAccessed) {
+          this.router.navigate(['/alreadyattended']);
+          return false;
+        } else if (!isValid) {
+          this.router.navigate(['/invalid']);
+          return false;
+        }
+
+        return true;
+      }),
+      catchError((error) => {
+        console.error('Error in guard:', error);
+        this.router.navigate(['/invalid']);
+        return of(false);
+      })
+    );
+  }
+
+  private isLinkExpired(expiryDate: string | Date): boolean {
+    const expiryDateObj = new Date(expiryDate);
+    return expiryDateObj < new Date();
+  }
+}
