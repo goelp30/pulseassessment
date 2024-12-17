ca17803a538547c55b75971c8f46712687efd325
Kashish <>
2024-12-17 15:59:45 +0530

    listentochangerwithfilter

--- src/app/components/assessment/drag-drop/drag-drop.component.ts
+++ src/app/components/assessment/drag-drop/drag-drop.component.ts
@@ -60,12 +60,10 @@ ngOnInit(): void {
       this.savedFormData = JSON.parse(savedData);
     }
   }
-
   this.subscribeToFormChanges();
   this.router.events.subscribe((event) => {
     if (event instanceof NavigationStart) {
       this.clearLocalStorageOnNavigation();
-
     }
   });
 
@@ -335,7 +333,7 @@ getEditData(assessmentId: string, isEditing: boolean): void {
           this.viewMode = assessment.assessmentType; // Populate viewMode
   
           // Fetch associated subjects and their ratings from the 'assessmentList' table
-          this.firebaseService.getItemsByFields(TableNames.AssessmentList, ['assessmentId'], this.assessmentId).subscribe((assessmentLists: any[]) => {
+          this.firebaseService.listensToChangeWithFilter(TableNames.AssessmentList, 'assessmentId', this.assessmentId).subscribe((assessmentLists: any[]) => {
             if (assessmentLists && assessmentLists.length > 0) {
               const assessmentList = assessmentLists[0]; // Assuming assessmentId is unique, get the first match
               const subjectsWithRatings = assessmentList.subjects; // subjects with ratings
@@ -613,7 +611,6 @@ this.addAssessment().then((assessmentId: string) => {
   this.toastr.error('Failed to create assessment. ' + error);
 });
 }
-// listen to changes added
 calculateQuestionCounts(subjectId: string): void {
   // Assuming `questions` is the array of question objects
   this.firebaseService.listensToChangeWithFilter('questions', 'subjectId', subjectId).subscribe((questions: any[]) => {
