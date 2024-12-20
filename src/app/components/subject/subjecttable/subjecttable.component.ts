98a5e8de6f90450f2c953c33bb734bf807230fcc
harshjdev <harsh97736@gmail.com>
2024-12-20 17:32:10 +0530

    buttons updated

--- src/app/components/subject/subjecttable/subjecttable.component.ts
+++ src/app/components/subject/subjecttable/subjecttable.component.ts
@@ -57,19 +57,19 @@ export class SubjectTableComponent  implements OnInit{
     {
       label: '',
       icon: 'fa fa-edit',  // Font Awesome Edit Icon
-      colorClass: 'bg-custom-blue hover:opacity-80 transition-opacity text-white py-2 px-4 text-white rounded-md text-xl ', 
+      colorClass: 'text-blue-900 text-2xl ', 
       action: (row: any) => this.editSubject(row),
     },    
     {
       label: '',
       icon: 'fas fa-cog',  // Font Awesome Manage Icon
-      colorClass: 'bg-custom-blue hover:opacity-80 transition-opacity text-white py-2 px-4 text-white rounded-md text-xl',
+      colorClass: 'text-blue-900 text-2xl',
       action: (row: any) => this.manageSubject(row),
     },
     {
       label: '',
       icon: 'fa fa-trash',  // Font Awesome Delete Icon
-      colorClass: 'bg-red-500 hover:opacity-80 transition-opacity py-2 px-4 text-white rounded-md text-xl',
+      colorClass: 'text-red-500 text-2xl',
       action: (row: any) => this.confirmDelete(row),
     },
   ];
