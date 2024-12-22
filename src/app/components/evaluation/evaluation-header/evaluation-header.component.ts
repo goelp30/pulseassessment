a955de63329ee12a359cd27b4dbaf74479a4e0a2
Bhupendra Pratap Maurya <bhupendra.maurya@admiralindia.com>
2024-12-22 11:35:48 +0530

    evaluation Ui updated issues solved

--- src/app/components/evaluation/evaluation-header/evaluation-header.component.ts
+++ src/app/components/evaluation/evaluation-header/evaluation-header.component.ts
@@ -14,4 +14,12 @@ export class EvaluationHeaderComponent {
   @Input() userMarks: number = 0;
   @Input() totalMarks: number = 0;
   @Input() result?: string;
+
+  calculateResult(): string {
+    if (this.totalMarks === 0) {
+       return 'Fail'; // Or handle it differently if no total marks
+    }
+    const passPercentage = 0.7; // 70%
+    return this.userMarks / this.totalMarks >= passPercentage ? 'Pass' : 'Fail';
+}
 }
